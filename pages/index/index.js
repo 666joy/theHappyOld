56a2fbd5c9f8b473b86ca6c5bdbff9f45f9b7e8d
unknown <unknown@example.com>
2021-05-15 20:15:38 +0800

    菜品识别
    目前主要的功能为：通过调用API来识别菜品

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,11 +5,45 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
+    currentTab:0,
+    flag:0,
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
+    indicatorDots:true,
+    imgUrls:[
+      'http://r.photo.store.qq.com/psc?/V53g3jj51xYYHN1aGTvl0344zM2EMq9T/45NBuzDIW489QBoVep5mcRT.EdV7iP8g4Bv*atLtk**lqlm69Z*aKd5ZpalrMbnfK2UBMnBSmZSur2uFJsXRebr9lxTFcHcom9BqAkpHkOs!/r',
+      'http://r.photo.store.qq.com/psc?/V53g3jj51xYYHN1aGTvl0344zM2EMq9T/45NBuzDIW489QBoVep5mcWkdkHA5iMDkDNZHI.7agV92KH5w9mrpEZFUwjoaa6ihPQhk*ay0F1GPDzYF2LcP1Y9BiW.u*Zt3eCY5gzyGquc!/r',
+      'http://r.photo.store.qq.com/psc?/V53g3jj51xYYHN1aGTvl0344zM2EMq9T/45NBuzDIW489QBoVep5mcWkdkHA5iMDkDNZHI.7agV8Q.UJSBB0.72J*lI6sup*YujRUKRDC*gQg8lt1i7f5zGm4FL2Otw598Ex1qeJ91qs!/r',
+      'http://r.photo.store.qq.com/psc?/V53g3jj51xYYHN1aGTvl0344zM2EMq9T/45NBuzDIW489QBoVep5mcWkdkHA5iMDkDNZHI.7agV9WIinKuh7T1.WzbJnii4fAzgineimbiH3X9VmGtsZIw1.0TtqOmzdibhVFEp1NHVk!/r',
+      'http://r.photo.store.qq.com/psc?/V53g3jj51xYYHN1aGTvl0344zM2EMq9T/45NBuzDIW489QBoVep5mcWkdkHA5iMDkDNZHI.7agV9s9Cl6X1VQHAJLmabV49Vdtj7Zf9nD9oPnPovAi9tDgF*Ypq24VFLwQ03AIgWa1D0!/r',
+      'http://r.photo.store.qq.com/psc?/V53g3jj51xYYHN1aGTvl0344zM2EMq9T/45NBuzDIW489QBoVep5mcQiNAW4hdlJO0lPPtZXENqZFOGLxN0F6VR2gkjdCIGSU0KE*lFzN*2tFOO9gtLexGcrrJbTXJ6w3ok4iZ*eb4ec!/r',
+    ]
+  },
+  seeDetail:function(e){//美食推荐轮播
+    var id = e.currentTarget.id;
+    wx.navigateTo({
+      url:'../subjectDetail/subjectDetail?id='+id
+    })
+  },
+  switchNav:function(e){
+    var page = this;
+    var id = e.target.dataset.current;//变换置顶标签
+    if(this.data.currentTab == id){
+       return false;
+    }else{
+      page.setData({currentTab:id});
+    }
+    page.setData({flag:id});
+  },
+  onShareAppMessage:function(){//分享页面
+    return{
+      title:'卡路里检测',
+      desc:'菜品识别',
+      path:'/pages/index/index.js'
+    }
   },
   // 事件处理函数
   bindViewTap() {
@@ -44,5 +78,10 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+  kaluli(){
+    wx.navigateTo({
+      url: '/pages/try-04/try-04?name1=value1&name2=value2',
+    })
+  },
 })
--- pages/try-03/try-03.js
+++ pages/try-03/try-03.js
@@ -1,66 +1,58 @@
 // pages/try-03/try-03.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    book:[],
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  putimg: function(){// 添加照片
+      let that=this;
+      let img=that.data.img;
+      let word=that.data.word;
+      let word1=that.data.word1;
+      wx.chooseImage({
+        success: function(res) {
+          that.setData({
+            img:res.tempFilePaths[0], //赋予img图片地址（img）
+            word: wx.getFileSystemManager().readFileSync(res.tempFilePaths[0], "base64"), //将图片转为base64（word）
+            word1: encodeURIComponent(wx.getFileSystemManager().readFileSync(res.tempFilePaths[0], "base64"))//将base64转为urlencode
+          })
+        },
+        count: 1,
+      });
+    },
+  
+    test: function(){ //获取百度api的access-token
+      let that = this;
+      let key =that.data.key;
+      wx.request({
+        url: 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=laVgfleAVTKrRUo8F9o31gGC&client_secret=YG3kUDg6C2HbkdmSwIRaq7QUlxPX84GN&',
+        header: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        success: function (res) {
+         that.setData({ key: res.data.access_token}) //获取access-token赋值为key
+        },
+        fail: function(){
+          console.log("fail")
+        }
+      })  
+    },
+    
+    test2: function(){
+      let that =this;
+      let key =that.data.key;
+      let word = that.data.word;
+      let word1 = that.data.word1;
+      let book = that.data.book;
+      wx.request({
+        url: "https://aip.baidubce.com/rest/2.0/image-classify/v2/dish?access_token="+key,
+        method: "POST",
+        data:{
+          image:word
+        },
+        header: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        success: function (res) {
+          that.setData({ book: res.data.result})
+          console.log(res.data.result);
+        },
+      })
+    },
+ 
 })
