60428753152b06cf3a4989e2f2f2ba4ffb9f08c9
lcy <1621042529@qq.com>
2023-03-10 00:49:51 +0800

    3月9日

---
+++ pages/apply/apply.js
@@ -0,0 +1,125 @@
+// pages/apply/apply.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // input默认是1
+		num: 1,
+		// 使用data数据对象设置样式名
+    minusStatus: 'disabled',
+    btn_disabled:true
+
+  },
+
+  /* 点击减号 */
+	bindMinus: function() {
+		var num = this.data.num;
+		// 如果大于1时，才可以减
+		if (num > 1) {
+			num --;
+		}
+		// 只有大于一件的时候，才能normal状态，否则disable状态
+		var minusStatus = num <= 1 ? 'disabled' : 'normal';
+		// 将数值与状态写回
+		this.setData({
+			num: num,
+			minusStatus: minusStatus
+		});
+  },
+  /* 点击加号 */
+	bindPlus: function() {
+		var num = this.data.num;
+		// 不作过多考虑自增1
+		num ++;
+		// 只有大于一件的时候，才能normal状态，否则disable状态
+		var minusStatus = num < 1 ? 'disabled' : 'normal';
+		// 将数值与状态写回
+		this.setData({
+			num: num,
+			minusStatus: minusStatus
+		});
+	},
+	/* 输入框事件 */
+	bindManual: function(e) {
+		var num = e.detail.value;
+		// 将数值与状态写回
+		this.setData({
+			num: num
+		});
+  },
+
+  bindAgreeChange:function(e) {
+  //  console.log(e.detail.value)
+    this.setData({
+      isAgree:e.detail.value.length,
+    })
+    if (e.detail.value.length==1){
+     this.setData({
+       btn_disabled:false,
+     })
+   }else{
+      //onsole.log(e.detail.value.length)
+     this.setData({
+       btn_disabled:true
+     })
+   }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
